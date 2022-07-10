698a0183f281195920d108dba95c5e6d7b5056d0
ledevduck <devduck@tutanota.com>
2022-07-10 13:58:08 +0200

    Inject the FormBuilder service in the CartComponent constructor()

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -11,5 +11,8 @@ import { CartService } from '../cart.service';
 export class CartComponent {
   items = this.cartService.getItems();
 
-  constructor(private cartService: CartService) {}
+  constructor(
+    private cartService: CartService,
+    private formBuilder: FormBuilder
+  ) {}
 }
