ef17ffb3932fbd18754f0b6c59bcde2f9cc68853
ledevduck <devduck@tutanota.com>
2022-07-10 13:48:39 +0200

    *Configuring the ShippingComponent to use CartService* modify import in shipping.component.ts

--- src/app/shipping/shipping.component.ts
+++ src/app/shipping/shipping.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Observable } from 'rxjs';
+import { CartService } from '../cart.service';
+
 @Component({
   selector: 'app-shipping',
   templateUrl: './shipping.component.html',
-  styleUrls: ['./shipping.component.css']
+  styleUrls: ['./shipping.component.css'],
 })
 export class ShippingComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
