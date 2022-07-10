5cf733885bffc14171ca1bcee89d0bc17036e6d6
ledevduck <devduck@tutanota.com>
2022-07-10 11:41:30 +0200

    Inject ActivatedRoute into the constructor() by adding private route: ActivatedRoute as an argument within the constructor's parentheses, in product-details.component.ts

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -11,7 +11,7 @@ import { Product, products } from '../products';
 export class ProductDetailsComponent implements OnInit {
   product: Product | undefined;
 
-  constructor() {}
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {}
 }
